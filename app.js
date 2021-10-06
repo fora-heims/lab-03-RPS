74fb5f28a4c759a0f0dca7d1009f86e168c85135
fora-heims <foraheims@gmail.com>
2021-10-06 16:59:33 -0700

    Cleaned up the comments on app.js

--- app.js
+++ app.js
@@ -9,44 +9,53 @@ const win = document.getElementById('wins-cell');
 const lose = document.getElementById('losses-cell');
 const draw = document.getElementById('draws-cell');
 
+const choices = ['rock', 'paper', 'scissors'];
+
 // initialize global state
 let wins = 0;
 let losses = 0;
 let draws = 0;
 let resets = 0;
-const choices = ['rock', 'paper', 'scissors'];
 
 // set event listeners 
 play.addEventListener('click', () => {
+    // get user input
     let userChoice = document.querySelector('input[type=radio]:checked').value;
+    // get computer throw function
     let compChoice = choices[Math.floor(Math.random() * choices.length)];
+    // use user input to update state
     let compare = doesUserWin(userChoice, compChoice);
     if (compare === 'draw') {
+        // update state 
         draws++;
+        // update DOM to reflect the new state
         draw.textContent = draws;
         message.textContent = 'It\'s a draw.';
     } else if (compare === 'win') {
+        // update state 
         wins++;
+        // update DOM to reflect the new state
         win.textContent = wins;
         message.textContent = 'You win!';
     } else if (compare === 'lose') {
+        // update state
         losses++;
+        // update DOM to reflect the new state
         lose.textContent = losses;
         message.textContent = 'You lose.';
     }
 });
 
+// set event listeners
 reset.addEventListener('click', () => {
+    // update state 
     resets++;
-    resetCount.textContent = resets;
     wins = 0;
     losses = 0;
     draws = 0;
+    // update DOM to reflect the new state
     win.textContent = wins;
     lose.textContent = losses;
     draw.textContent = draws;
+    resetCount.textContent = resets;
 });
-
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
