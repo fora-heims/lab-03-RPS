ed95092f1018106d81d52b1226c11fe51fc0fba9
fora-heims <foraheims@gmail.com>
2021-10-06 14:42:31 -0700

    Defined some of the variables, and some of the states. Not finished with that yet.

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 // import functions and grab DOM elements
+import { doesUserWin } from './utili.js';
+
+const rock = document.getElementById('rock-radio');
+const scissors = document.getElementById('scissors-radio');
+const paper = document.getElementById('paper-radio');
+const play = document.getElementById('play-button');
+const reset = document.getElementById('reset-button');
+
 
 // initialize global state
+let wins = 0;
+let losses = 0;
+let draws = 0;
+let resets = 0;
 
 // set event listeners 
   // get user input
