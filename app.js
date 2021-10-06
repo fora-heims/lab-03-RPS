bdc05c1b72ab8b24f24fcdfec67862a4ffce5835
fora-heims <foraheims@gmail.com>
2021-10-06 16:45:50 -0700

    It works! Play button event compares user selected choice with random computer choice. Displays result of that game. Score is tracked in a table. Reset Score button functions too. Also keeps track of how many times score has been reset.

--- app.js
+++ app.js
@@ -1,13 +1,10 @@
 // import functions and grab DOM elements
 import { doesUserWin } from './utili.js';
 
-const rock = document.getElementById('rock-radio');
-const scissors = document.getElementById('scissors-radio');
-const paper = document.getElementById('paper-radio');
 const play = document.getElementById('play-button');
 const reset = document.getElementById('reset-button');
 const message = document.getElementById('message');
-const resetCount = document.getElementById('reset-button');
+const resetCount = document.getElementById('reset-count');
 const win = document.getElementById('wins-cell');
 const lose = document.getElementById('losses-cell');
 const draw = document.getElementById('draws-cell');
@@ -17,14 +14,37 @@ let wins = 0;
 let losses = 0;
 let draws = 0;
 let resets = 0;
+const choices = ['rock', 'paper', 'scissors'];
 
 // set event listeners 
 play.addEventListener('click', () => {
-  let userChoice = document.querySelector('input[type=radio]:checked');
+    let userChoice = document.querySelector('input[type=radio]:checked').value;
+    let compChoice = choices[Math.floor(Math.random() * choices.length)];
+    let compare = doesUserWin(userChoice, compChoice);
+    if (compare === 'draw') {
+        draws++;
+        draw.textContent = draws;
+        message.textContent = 'It\'s a draw.';
+    } else if (compare === 'win') {
+        wins++;
+        win.textContent = wins;
+        message.textContent = 'You win!';
+    } else if (compare === 'lose') {
+        losses++;
+        lose.textContent = losses;
+        message.textContent = 'You lose.';
+    }
 });
 
 reset.addEventListener('click', () => {
-
+    resets++;
+    resetCount.textContent = resets;
+    wins = 0;
+    losses = 0;
+    draws = 0;
+    win.textContent = wins;
+    lose.textContent = losses;
+    draw.textContent = draws;
 });
 
   // get user input
--- test/example.test.js
+++ test/example.test.js
@@ -4,31 +4,31 @@ import { doesUserWin } from '../utili.js';
 const test = QUnit.test;
 
 test('Draw - user === comp', (expect) => {
-    const expected = 'It\'s a draw';
+    const expected = 'draw';
     const actual = doesUserWin('rock', 'rock');
     expect.equal(actual, expected);
 });
 
 test('Win - user-paper v comp-rock', (expect) => {
-    const expected = 'You win!';
+    const expected = 'win';
     const actual = doesUserWin('paper', 'rock');
     expect.equal(actual, expected);
 });
 
 test('Win - user-scissors v comp-paper', (expect) => {
-    const expected = 'You win!';
+    const expected = 'win';
     const actual = doesUserWin('scissors', 'paper');
     expect.equal(actual, expected);
 });
 
 test('Win - user-rock v comp-scissors', (expect) => {
-    const expected = 'You win!';
+    const expected = 'win';
     const actual = doesUserWin('rock', 'scissors');
     expect.equal(actual, expected);
 });
 
 test('Lose', (expect) => {
-    const expected = 'You lose.';
+    const expected = 'lose';
     const actual = doesUserWin('rock', 'paper');
     expect.equal(actual, expected);
 });
